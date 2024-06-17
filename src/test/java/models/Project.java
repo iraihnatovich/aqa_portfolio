343cdfb79ff875a25a294a22ac23e3edb32ab94d
iihnatovich <i.ihnatovich@gmail.com>
2024-06-17 13:59:44 +0300

    models

--- src/test/java/models/Project.java
+++ src/test/java/models/Project.java
@@ -1,4 +1,15 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@Data
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Project {
+    @EqualsAndHashCode.Exclude
+    private int id;
+    private String name;
 }
