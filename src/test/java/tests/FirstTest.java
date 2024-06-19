bdf5e4df591dea6680785a562bf1033519d9ec43
iihnatovich <i.ihnatovich@gmail.com>
2024-06-19 13:46:20 +0300

    ci_cd check

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -1,4 +1,12 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class FirstTest {
+    @Test
+    public void cicdTest() {
+        System.out.println("ci cd test");
+        Assert.assertTrue(true);
+    }
 }
